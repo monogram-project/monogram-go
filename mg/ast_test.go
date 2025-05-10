801bf9b691931b6abfc4c7ebef2842c17a923d4b
Stephen Leach <sfkleach@gmail.com>
2025-05-10 10:47:44 +0100

    Fix the ast_test that parses the functests

--- mg/ast_test.go
+++ mg/ast_test.go
@@ -38,7 +38,7 @@ func CheckTranslationToAST(input string) error {
 
 func TestAST(t *testing.T) {
 	// Load the YAML file - we run from go/monogram/lib
-	data, err := os.ReadFile("../../../functests/xml-tests.yaml")
+	data, err := os.ReadFile("../functests/xml-tests.yaml")
 	if err != nil {
 		t.Fatalf("Failed to read YAML file: %v", err)
 	}
