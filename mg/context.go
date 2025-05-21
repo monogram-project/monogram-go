59d7ccbca2b52a8114e3b6d17876a8420932ba4d
Stephen Leach <sfkleach@gmail.com>
2025-05-21 10:46:07 +0100

    Refactoring contexts and fixing bug

---
+++ mg/context.go
@@ -0,0 +1,26 @@
+package mg
+
+type Context struct {
+	AcceptNewline bool
+	InsideForm    bool
+}
+
+func makeContext() Context {
+	return Context{AcceptNewline: false}
+}
+
+func (c Context) setInsideForm(insideForm bool) Context {
+	c.AcceptNewline = insideForm
+	c.InsideForm = insideForm
+	return c
+}
+
+func (c Context) setAcceptNewline(acceptNewline bool) Context {
+	c.AcceptNewline = acceptNewline
+	return c
+}
+
+func (c Context) setInsideBraces(insideBraces bool) Context {
+	c.AcceptNewline = insideBraces
+	return c.setInsideForm(false).setAcceptNewline(insideBraces)
+}
