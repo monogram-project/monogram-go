e7d260d93efa0df85911d3b0c55d76869e4bc2b7
Stephen Leach <sfkleach@gmail.com>
2025-08-03 14:50:40 +0100

    Improve code coverage of unit tests

--- mg/ast_test.go
+++ mg/ast_test.go
@@ -37,6 +37,70 @@ func CheckTranslationToAST(input string) error {
 	return nil
 }
 
+// CheckTranslationToTypedAST tests the strongly-typed AST API
+func CheckTranslationToTypedAST(input string) error {
+	p_opts := NewParserOptions()
+	element, err := p_opts.ParseToElement(input, "", false)
+	if err != nil {
+		return err
+	}
+
+	// Exercise the strongly-typed AST API by traversing it
+	return walkElement(element)
+}
+
+// walkElement recursively walks through a strongly-typed AST element
+// exercising the library API methods
+func walkElement(element Element) error {
+	// Test basic Element interface methods
+	_ = element.Name()
+	_ = element.ChildrenCount()
+	_ = element.FromTo()
+
+	// Test option access methods using iterator
+	options := element.Options()
+	for options.Next() {
+		key := options.Value()
+		_ = element.HasOption(key)
+		_ = element.GetOption(key)
+	}
+
+	// Test children iteration using the iterator interface
+	children := element.Children()
+	for children.Next() {
+		child := children.Value()
+		err := walkElement(child)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Test type-specific methods by attempting type assertions
+	switch node := element.(type) {
+	case *NumberNode:
+		_ = node.Value // Access the typed field
+	case *IdentifierNode:
+		_ = node.Id // Access the typed field
+	case *InfixOperatorNode:
+		_ = node.Op  // Access the typed field
+		_ = node.LHS // Access the typed field
+		_ = node.RHS // Access the typed field
+	case *PrefixOperatorNode:
+		_ = node.Op  // Access the typed field
+		_ = node.Arg // Access the typed field
+	case *StringNode:
+		_ = node.Value // Access the typed field
+		_ = node.Quote // Access the typed field
+	case *FormNode:
+		_ = node.Name // Access the typed field
+	case *ApplyNode:
+		_ = node.Func // Access the typed field
+		_ = node.Args // Access the typed field
+	}
+
+	return nil
+}
+
 func TestAST(t *testing.T) {
 	// Use glob to find all YAML files in the functests directory
 	yamlFiles, err := filepath.Glob("../functests/*.yaml")
@@ -72,6 +136,14 @@ func TestAST(t *testing.T) {
 						err := CheckTranslationToAST(testCase.Input)
 						if err != nil {
 							t.Errorf("Cannot parse test-case '%s' from file '%s': %v", testCase.Name, fileName, err)
+						}
+					})
+
+					// Also test the strongly-typed AST
+					t.Run(testCase.Name+"_typed", func(t *testing.T) {
+						err := CheckTranslationToTypedAST(testCase.Input)
+						if err != nil {
+							t.Errorf("Cannot parse to typed AST test-case '%s' from file '%s': %v", testCase.Name, fileName, err)
 						}
 					})
 				}
